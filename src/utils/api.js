58996cee694df23346a747fcfed2af21b262e0ed
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-17 15:18:52 -0700

    add proxy, error handling

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,15 +4,22 @@ import request from 'superagent';
 export async function signUp(credentials) {
   const response = await request
     .post('/api/auth/signup')
+    .ok(res => res.status < 500)
     .send(credentials);
-
+  if (response.status === 400) {
+    throw response.body;
+  }
   return response.body;
 }
 
 export async function signIn(credentials) {
   const response = await request
     .post('/api/auth/signin')
+    .ok(res => res.status < 500)
     .send(credentials);
+  if (response.status === 400) {
+    throw response.body;
+  }
 
   return response.body;
 }
