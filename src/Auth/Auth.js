4e1fbadd921dea65f27c23bce6acb12933ada850
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-17 14:38:45 -0700

    handle events in auth

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,11 +3,21 @@ import './Auth.css';
 
 export default class Auth extends Component {
   state = {
-    isSignUp: true
+    isSignUp: true,
+    username: '',
+    password: ''
   }
 
   handleSwitch = () => {
     this.setState({ isSignUp: !this.state.isSignUp });
+  }
+
+  handleUsernameChange = ({ target }) => {
+    this.setState({ username: target.value });
+  }
+
+  handlePasswordChange = ({ target }) => {
+    this.setState({ password: target.value });
   }
 
   handleSubmit = e => {
@@ -16,13 +26,18 @@ export default class Auth extends Component {
 
 
   render() {
-    const { isSignUp } = this.state;
+    const { isSignUp, username, password } = this.state;
     return (
       <form className="login" onSubmit={this.handleSubmit}>
         <h3>Log in/Sign up!</h3>
-        <input name="username" placeholder="username"></input>
-        <input name="password" placeholder="password" type='password'></input>
-        <button onClick={this.handleSwitch} className="login button">Sign {isSignUp ? 'Up' : 'In'}</button>  
+        <input name="username" placeholder="username" value={username} onChange={this.handleUsernameChange} required={true}></input>
+        <input name="password" placeholder="password" type='password' value={password} onChange={this.handlePasswordChange} required={true}></input>
+        <button onClick={this.handleSwitch} className="login button">Sign { isSignUp ? 'Up' : 'In'}</button>  
+        {/* <button onClick={this.handleSwitch}>
+          {isSignUp
+            ? 'Already Have An Account?'
+            : 'Click here to log in'}
+        </button> */}
       </form>
     );
   }
